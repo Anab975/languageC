f2f2da529c8109db0de25bef0e9081f079366a15
Ana Paula Brasil <143728953+Anab975@users.noreply.github.com>
2025-01-13 01:39:07 +0000

    novo exercicio

---
+++ exercicio02/media_alunos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+    float nota01, nota02, media;
+
+    nota01 = 9.5;
+    nota02 = 7.3;
+
+    media = (nota01 + nota02) / 2;
+
+    printf("A media do Aluno e: %.2f", media);
+
+    return 0;
+}
